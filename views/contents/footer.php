956a328cf552633add980d3db441a4784dbe1bf4
Kenan <kea.aksoy@gmail.com>
2024-09-08 01:56:32 +0300

    footer ayrıştırıldı.

---
+++ views/contents/footer.php
@@ -0,0 +1,35 @@
+<footer>
+    <div class="footer-container">
+
+        <div class="logo">
+            <i class="fa-solid fa-share-nodes fa-xl"></i>    
+            <div class="markname">
+                <span><a href="AnaSayfa">LogoRitmik</a></span>
+                <div class="line"></div> 
+            </div>
+            
+            
+        </div>
+
+        <div class="slogan">            
+            <p>Bilginin ritmiyle şekillenen bir dünyada, düşüncenin ve keşfin izini sürün.</p>
+        </div>
+
+
+            <div id="mail_listesi">
+                <h5>Mail Listemize Abone Olun:  </h5>
+                <form id="mail_listesi_form" action="post">
+                    <input id="eposta" type="mail" placeholder="E-Posta adresinizi girin">
+                    <input class="btn greenfade" type="button" value="Onayla">
+                </form>
+                <span>Onaylandığında <a class="bluefade" href="#" target="_blank" rel="noopener noreferrer">Gizlilik Politikası</a> kabul edilmiş olur.</span>
+           
+        </div>
+    </div>
+
+    <div class="dip">
+        <p>Her Hakkı Saklıdır ve Logoritmik.com'a aittir.</p> <br>
+        <p>Bu siteye ait tüm kodlamalar ve içerikler özgün olup <a class="primaryfade" href="http://logoritmik.com" target="_blank" rel="noopener noreferrer">©Logoritmik.com</a> tarafından yapılmıştır.</p>
+    </div>
+
+</footer>

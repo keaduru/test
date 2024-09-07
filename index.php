0bea1b2e2834eceea8884f558622df9b9204a713
Kenan <kea.aksoy@gmail.com>
2024-09-08 01:53:17 +0300

    head.php ayrıştıırldı

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <!DOCTYPE html>
 <html lang="tr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kurulum Dosyası Jquery</title>
-    <!--<script src="https://kit.fontawesome.com/49445b394c.js" crossorigin="anonymous"></script> -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="Style/style.css">
-    <link rel="stylesheet" href="Assets/fontawasome/css/all.min.css">
-    <script src="Assets/jqery/jquery.js"></script>
-    <script src="js/app.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> 
-
-
-</head>
+<?php require "views/contents/head.php"?>
 
 <body class="medium-font">
 
